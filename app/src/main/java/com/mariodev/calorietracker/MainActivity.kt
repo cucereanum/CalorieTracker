eb46a227bb882586745998063a815f25e03f6dc6
cucereanum <cucereanum@yahoo.com>
2023-05-25 21:53:18 +0300

    Create the 'Search' screen and viewModel

--- app/src/main/java/com/mariodev/calorietracker/MainActivity.kt
+++ app/src/main/java/com/mariodev/calorietracker/MainActivity.kt
@@ -8,11 +8,14 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.mariodev.calorietracker.navigation.navigate
 import com.mariodev.calorietracker.ui.theme.CalorieTrackerTheme
+import com.mariodev.core.domain.preferences.Preferences
 import com.mariodev.core.navigation.Route
 import com.mariodev.onboarding_presentation.activity.ActivityScreen
 import com.mariodev.onboarding_presentation.age.AgeScreen
@@ -22,14 +25,21 @@ import com.mariodev.onboarding_presentation.height.HeightScreen
 import com.mariodev.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.mariodev.onboarding_presentation.weight.WeightScreen
 import com.mariodev.onboarding_presentation.welcome.WelcomeScreen
+import com.mariodev.tracker_presentation.search.SearchScreen
 import com.mariodev.tracker_presentation.tracker_overview.TrackerOverviewScreen
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var preferences: Preferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val shouldShowOnboarding = preferences.loadShouldShowOnboarding()
         setContent {
             CalorieTrackerTheme {
                 val navController = rememberNavController()
@@ -40,7 +50,9 @@ class MainActivity : ComponentActivity() {
                 ) {
                     NavHost(
                         navController = navController,
-                        startDestination = Route.WELCOME
+                        startDestination = if (shouldShowOnboarding) {
+                            Route.WELCOME
+                        } else Route.TRACKER_OVERVIEW
                     ) {
                         composable(Route.WELCOME) {
                             WelcomeScreen(onNavigate = navController::navigate)
@@ -82,8 +94,34 @@ class MainActivity : ComponentActivity() {
                         composable(Route.TRACKER_OVERVIEW) {
                             TrackerOverviewScreen(onNavigate = navController::navigate)
                         }
-                        composable(Route.SEARCH) {
-
+                        composable(route = Route.SEARCH + "/{mealName}/{dayOfMonth}/{month}/{year}",
+                            arguments = listOf(
+                                navArgument("mealName") {
+                                    type = NavType.StringType
+                                },
+                                navArgument("dayOfMonth") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("month") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("year") {
+                                    type = NavType.IntType
+                                }
+                            )) {
+                            val mealName = it.arguments?.getString("mealName")!!
+                            val dayOfMonth = it.arguments?.getInt("dayOfMonth")!!
+                            val month = it.arguments?.getInt("month")!!
+                            val year = it.arguments?.getInt("year")!!
+                            SearchScreen(
+                                scaffoldState = scaffoldState,
+                                mealName = mealName,
+                                dayOfMonth = dayOfMonth,
+                                month = month,
+                                year = year,
+                                onNavigateUp = {
+                                    navController.navigateUp()
+                                })
                         }
                     }
                 }
